d8a2310d4992fd0ed06a87dbbc87e54d35f0db47
Lucas B <lucas.brandy@laposte.net>
2022-04-09 15:11:03 +0200

    controller accueil

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccueilController extends AbstractController
+{
+    #[Route('/accueil', name: 'app_accueil')]
+    /**
+     * @Route("", name = "accueil")
+     */
+    public function index(): Response
+    {
+        return new Response("
+            <body>
+                <p>Bienvenu sur notre Site Symfony !</p>
+            </body>
+            ");
+    }
+}
