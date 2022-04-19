805d633164cc2770c95d4149bafc7948987b36de
Lucas B <lucas.brandy@laposte.net>
2022-04-19 17:13:13 +0200

    accueil admin

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -6,14 +6,45 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\User;
+/**
+ * @Route("/admin", name = "admin")
+ */
+
 class AdminController extends AbstractController
 {
-    #[Route('/admin/admin', name: 'app_admin_admin')]
+
+    /**
+     * @Route("/index", name = "_index")
+     */
     public function index(): Response
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/Admin/AdminController.php',
         ]);
+    }
+
+    /**
+     * @Route("/accueil/{id}", name = "_accueil")
+     */
+    public function accueilAdmin(ManagerRegistry $doctrine,$id): Response {
+
+        $em = $doctrine->getManager();
+
+        $filmRepository = $em->getRepository('App:User');
+
+        $user = $filmRepository->find($id);
+
+        $args = array(
+            'type' => 'Admin',
+            'name' => $user->getName(),
+            'firstname' => $user->getFirstName(),
+        );
+
+        return $this->render('layout/accueil.html.twig', $args);
+
     }
 }
